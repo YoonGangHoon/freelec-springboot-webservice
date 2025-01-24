e05a69656992528206edad6798afc08377f069a5
YoonGangHoon <yghun021007@naver.com>
2025-01-24 17:30:15 +0900

    DTO 생성

---
+++ src/main/java/com/jojoldu/book/freelec_springboot_webservice/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.freelec_springboot_webservice.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
