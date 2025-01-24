933f7e91a44de758ea47b4a956b7deb7864b3210
YoonGangHoon <yghun021007@naver.com>
2025-01-24 17:31:16 +0900

    Test 생성

---
+++ src/test/java/com/jojoldu/book/freelec_springboot_webservice/controller/HelloControllerTest.java
@@ -0,0 +1,44 @@
+package com.jojoldu.book.freelec_springboot_webservice.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(controllers = HelloController.class)
+class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void hello가_리턴된다() throws Exception {
+        // Given
+        String hello = "hello";
+
+        // When & Then
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+
+    @Test
+    void helloDto가_리턴된다() throws Exception {
+        // given
+        String name = "hello";
+        int amount = 1000;
+
+        // when & then
+        mvc.perform(
+                        get("/hello/dto")
+                                .param("name", name)
+                                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", name).exists())
+                .andExpect(jsonPath("$.amount", amount).exists());
+
+    }
+}
