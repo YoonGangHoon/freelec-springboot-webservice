1f15f4d7cbbfc609bff5faffbea83e6248904cfd
YoonGangHoon <yghun021007@naver.com>
2025-01-24 15:22:06 +0900

    Initial Project

---
+++ src/test/java/com/jojoldu/book/freelec_springboot_webservice/FreelecSpringbootWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.freelec_springboot_webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelecSpringbootWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
