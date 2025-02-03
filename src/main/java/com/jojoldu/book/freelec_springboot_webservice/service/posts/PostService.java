533897a64694270a5351c9b1ae74be21cbedc1cc
YoonGangHoon <yghun021007@naver.com>
2025-02-03 17:16:00 +0900

    게시글 CRUD

--- src/main/java/com/jojoldu/book/freelec_springboot_webservice/service/posts/PostService.java
+++ src/main/java/com/jojoldu/book/freelec_springboot_webservice/service/posts/PostService.java
@@ -2,12 +2,16 @@ package com.jojoldu.book.freelec_springboot_webservice.service.posts;
 
 import com.jojoldu.book.freelec_springboot_webservice.domain.posts.Posts;
 import com.jojoldu.book.freelec_springboot_webservice.domain.posts.PostsRepository;
+import com.jojoldu.book.freelec_springboot_webservice.dto.PostsListResponseDto;
 import com.jojoldu.book.freelec_springboot_webservice.dto.PostsResponseDto;
 import com.jojoldu.book.freelec_springboot_webservice.dto.PostsSaveRequestDto;
 import com.jojoldu.book.freelec_springboot_webservice.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,8 +36,23 @@ public class PostService {
 
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity); // entity를 직접 보낼 수 없기 때문에 DTO를 만들어서 리턴해 줌
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("This post does not exist. id=" + id));
+        postsRepository.delete(posts);
     }
 }
