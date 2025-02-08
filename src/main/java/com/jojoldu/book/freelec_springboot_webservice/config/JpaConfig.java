95efe407167a8a2f006e05e94307703065cd150e
YoonGangHoon <yghun021007@naver.com>
2025-02-08 17:35:49 +0900

    JPA 설정

---
+++ src/main/java/com/jojoldu/book/freelec_springboot_webservice/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.jojoldu.book.freelec_springboot_webservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
